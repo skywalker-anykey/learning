4935958481e21ac3630b10a73c254ea72e373e77
Т Н <ntolik@gmail.com>
2024-02-28 13:17:14 +0300

    add httprouter

--- src/web/go.mod
+++ src/web/go.mod
@@ -2,4 +2,4 @@ module web
 
 go 1.22.0
 
-require github.com/russross/blackfriday v1.6.0
+require github.com/julienschmidt/httprouter v1.3.0
--- src/web/main.go
+++ src/web/main.go
@@ -1,18 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/russross/blackfriday"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":8080", nil)
+	r := httprouter.New()
+	r.GET("/", HomeHandler)
+
+	// Posts collection
+	r.GET("/posts", PostsIndexHandler)
+	r.POST("/posts", PostsCreateHandler)
+
+	// Posts singular
+	r.GET("/posts/:id", PostShowHandler)
+	r.PUT("/posts/:id", PostUpdateHandler)
+	r.GET("/posts/:id/edit", PostEditHandler)
+
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
+
 }
 
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-	rw.Write(markdown)
+func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "Home")
+}
+
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "posts index")
+}
+
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "posts create")
+}
+
+func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	fmt.Fprintln(rw, "showing post", id)
+}
+
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post update")
+}
+
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post delete")
+}
+
+func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post edit")
 }
