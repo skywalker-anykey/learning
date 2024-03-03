aa8f20b0977c6440e25f68bc75f3c88387c530f1
Т Н <ntolik@gmail.com>
2024-03-03 11:32:52 +0300

    electronic

---
+++ src/module8/electronic/electronic.go
@@ -0,0 +1,95 @@
+package electronic
+
+type Phone interface {
+	Brand() string
+	Model() string
+	Type() string
+}
+
+type StationPhone interface {
+	ButtonsCount() int
+}
+
+type Smartphone interface {
+	OS() string //название операционной системы
+}
+
+// Добавьте в пакет electronic структуру applePhone. Сделайте так, чтобы она реализовывала интерфейсы Phone и Smartphone.
+type applePhone struct {
+	model string
+	os    string
+}
+
+func (a *applePhone) Brand() string {
+	return "apple"
+}
+
+func (a *applePhone) Model() string {
+	return a.model
+}
+
+func (a *applePhone) Type() string {
+	return "smartphone"
+}
+
+func (a *applePhone) OS() string {
+	return a.os
+}
+
+func NewApplePhone(model, os string) *applePhone {
+	return &applePhone{model: model, os: os}
+}
+
+// Добавьте в пакет electronic структуру androidPhone. Сделайте так, чтобы она также реализовывала интерфейсы Phone и Smartphone.
+type androidPhone struct {
+	brand string
+	model string
+	os    string
+}
+
+func (a *androidPhone) Brand() string {
+	return a.brand
+}
+
+func (a *androidPhone) Model() string {
+	return a.model
+}
+
+func (a *androidPhone) Type() string {
+	return "smartphone"
+}
+
+func (a *androidPhone) OS() string {
+	return a.os
+}
+
+func NewAndroidPhone(brand, model, os string) androidPhone {
+	return androidPhone{brand: brand, model: model, os: os}
+}
+
+// Добавьте в пакет electronic структуру radioPhone. Сделайте так, чтобы она реализовывала интерфейсы Phone и StationPhone.
+type radioPhone struct {
+	brand        string
+	model        string
+	buttonsCount int
+}
+
+func (r *radioPhone) Brand() string {
+	return r.brand
+}
+
+func (r *radioPhone) Model() string {
+	return r.model
+}
+
+func (r *radioPhone) Type() string {
+	return "station"
+}
+
+func (r *radioPhone) ButtonsCount() int {
+	return r.buttonsCount
+}
+
+func NewRadioPhone(brand string, model string, buttons int) radioPhone {
+	return radioPhone{brand: brand, model: model, buttonsCount: buttons}
+}
--- src/module8/main.go
+++ src/module8/main.go
@@ -1,15 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"module8/electronic"
+)
 
 func main() {
-	var input int
-	var f int = 1
-	fmt.Scanln(&input)
+	apple := electronic.NewApplePhone("iPhone15", "iOS 17.0")
+	android := electronic.NewAndroidPhone("POCO", "X6 Pro", "Android 15")
+	radio := electronic.NewRadioPhone("Panasonic", "T1000", 10)
 
-	for ; 0 < input; input-- {
-		f = f * input
+	printCharacteristics(&apple)
+	printCharacteristics(&android)
+	printCharacteristics(&radio)
+}
+
+// printCharacteristics должна выводить бренд, модель и тип телефона.
+func printCharacteristics(p electronic.Phone) {
+	fmt.Println("Brand:\t", p.Brand())
+	fmt.Println("Model:\t", p.Model())
+	fmt.Println("Type:\t", p.Type())
+	switch p.Type() {
+	case "station":
+		fmt.Println("ButtonsCount:\t", p.ButtonsCount())
+	case "smartphone":
+		fmt.Println("OS:\t", p.OS())
 	}
-
-	fmt.Println(f)
 }
