679c2104500e820b4697f9ab9a4a4cfede01da55
Т Н <ntolik@gmail.com>
2024-03-15 16:01:22 +0300

    factorial

--- src/my/main.go
+++ src/my/main.go
@@ -3,14 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// DO NOT delete or modify the code block below, it reads a random input word
-	var word string
-	//fmt.Scan(&word)
-	word = "uncopyrightable"
+	var f, num int
+	f, num = 1, 5
 
-	// Use slice expressions with the len() function
-	// to remove all the letters from 'word' except for the last letter.
-	word = word[len(word)-1:]
-
-	fmt.Println(word)
+	for i := 1; i <= num; i++ {
+		f *= i
+	}
+	fmt.Println(f)
 }
