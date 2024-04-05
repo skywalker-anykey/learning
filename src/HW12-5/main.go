67db0e19856920518a16c6297e9cf9e671bd931b
Т Н <ntolik@gmail.com>
2024-04-05 13:43:52 +0300

    HW12-5

---
+++ src/HW12-5/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano()) // необходимо для того, чтобы рандом был похож на рандомный
+}
+
+func main() {
+	ar := make([]int, 50)
+	for i := range ar {
+		ar[i] = rand.Intn(200) - 100 // ограничиваем случайно значение от [-100;100]
+	}
+
+	insertionSort(ar)
+
+	fmt.Println(ar)
+}
+
+func insertionSort(ar []int) {
+	// ваш код здесь
+}
