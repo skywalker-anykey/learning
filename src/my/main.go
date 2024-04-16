0f4922c83ed2d43536dbaaba985e775050f95394
Т Н <ntolik@gmail.com>
2024-04-16 12:24:59 +0300

    route

--- src/my/main.go
+++ src/my/main.go
@@ -1,16 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// first you need to change a stack
+// to store the coordinates in []int variable
+type XYStack struct {
+	storage [][]int
+}
+
+func (s *XYStack) Push(value []int) {
+	s.storage = append(s.storage, value)
+}
+
+func (s *XYStack) Pop() ([]int, error) {
+	last := len(s.storage) - 1
+	if last <= -1 {
+		return []int{0}, errors.New("stack is empty")
+	}
+
+	value := s.storage[last]
+	s.storage = s.storage[:last]
+
+	return value, nil
+}
 
 func main() {
-	var numbers = []int{1, 2, 3}
-	var index int
+	// don't worry about the input data
+	// the readLayout() function will do it for you (don't touch it!)
+	layout, start, finish := readLayout()
 
-	fmt.Scan(&index)
+	fmt.Println(ifPathExists(layout, start, finish))
+}
 
-	if index >= len(numbers) || index < 0 {
-		fmt.Println("prevented!")
-	} else {
-		fmt.Println(numbers[index])
+func ifPathExists(layout [][]int, start []int, finish []int) bool {
+	var solver XYStack
+
+	solver.Push(start)
+
+	for {
+		tile, err := solver.Pop() // get the last tile to check
+
+		if err != nil {
+			return false // the journey is over
+		}
+
+		x := tile[0]
+		y := tile[1]
+
+		if x == finish[0] && y == finish[1] {
+			return true
+		}
+
+		// if the lower path is clear
+		if y+1 < len(layout)-1 && layout[y+1][x] != 8 {
+			// we're moving down
+			solver.Push([]int{x, y + 1}) // add the tile to solver
+		}
+		// if the upper path is clear
+		if y-1 >= 0 && layout[y-1][x] != 8 {
+			// we're moving up
+			solver.Push([]int{x, y - 1})
+		}
+		// if the right path is clear x+1
+		if x+1 < len(layout)-1 && layout[y][x+1] != 8 {
+			solver.Push([]int{x + 1, y})
+		}
+
+		// if the left path is clear x-1
+		if x-1 >= 0 && layout[y][x-1] != 8 {
+			solver.Push([]int{x - 1, y})
+		}
+
+		// we need to mark the tiles where we were
+		layout[y][x] = 8 // let it be the wall
 	}
 }
