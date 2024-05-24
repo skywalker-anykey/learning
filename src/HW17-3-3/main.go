4e00644b40bbe6cbe1807d592aa6fa1864c2bd2b
Т Н <ntolik@gmail.com>
2024-05-24 16:41:04 +0300

    17.3.3 есть баг

--- src/HW17-3-3/main.go
+++ src/HW17-3-3/main.go
@@ -6,26 +6,60 @@ package main
 import (
 	"fmt"
 	"sync"
-	"sync/atomic"
 )
 
-const step int64 = 1
-const interationAmount int = 1000
-
 func main() {
-	var counter int64 = 0
+	const step int64 = 1
+	const interationAmount int64 = 100
+
+	var counter int64
 	var c = sync.NewCond(&sync.Mutex{})
-	increment := func(i int) {
-		atomic.AddInt64(&counter, step)
-		if i == interationAmount {
-			c.Signal()
+
+	increment := func() {
+		for {
+			c.L.Lock()
+			c.Wait()
+			counter = counter + step
+			fmt.Println(counter)
+			c.L.Unlock()
 		}
 	}
-	for i := 1; i <= interationAmount; i++ {
-		go increment(i)
+
+	go increment()
+	go increment()
+	go increment()
+	go increment()
+	go increment()
+	go increment()
+	go increment()
+	go increment()
+
+	for {
+		c.L.Lock()
+		if counter >= interationAmount {
+			break
+		}
+		c.Signal()
+		c.L.Unlock()
 	}
-	c.L.Lock()
-	c.Wait()
-	c.L.Unlock()
+
 	fmt.Println(counter)
 }
+
+//func main() {
+//	var counter int64 = 0
+//	var c = sync.NewCond(&sync.Mutex{})
+//	increment := func(i int) {
+//		atomic.AddInt64(&counter, step)
+//		if i == interationAmount {
+//			c.Signal()
+//		}
+//	}
+//	for i := 1; i <= interationAmount; i++ {
+//		go increment(i)
+//	}
+//	c.L.Lock()
+//	c.Wait()
+//	c.L.Unlock()
+//	fmt.Println(counter)
+//}
