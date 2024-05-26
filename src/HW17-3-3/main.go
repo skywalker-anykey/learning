c4daf3fc37b77bc8677bc7ced5b33b465215d1f1
Т Н <ntolik@gmail.com>
2024-05-26 19:20:07 +0300

    Вариант решения 17.3.3

--- src/HW17-3-3/main.go
+++ src/HW17-3-3/main.go
@@ -1,6 +1,3 @@
-// Перепишите приведённый выше пример со счётчиком из основного текста,
-// но вместо примитивов из пакета atomic используйте условную переменную и
-// попробуйте реализовать динамическую проверку достижения конечного значения счётчиком.
 package main
 
 import (
@@ -8,58 +5,31 @@ import (
 	"sync"
 )
 
-func main() {
-	const step int64 = 1
-	const interationAmount int64 = 100
+const step int64 = 1
+const interationAmount int = 1000
 
-	var counter int64
+func main() {
+	var counter int64 = 0
 	var c = sync.NewCond(&sync.Mutex{})
 
-	increment := func() {
-		for {
-			c.L.Lock()
-			c.Wait()
-			counter = counter + step
-			fmt.Println(counter)
-			c.L.Unlock()
-		}
+	// Запускаем 8 горутин
+	for i := 0; i < 8; i++ {
+		go func() {
+			for {
+				c.L.Lock()
+				if counter == int64(interationAmount) {
+					c.Signal()
+				} else {
+					counter = counter + step
+				}
+				c.L.Unlock()
+			}
+		}()
 	}
 
-	go increment()
-	go increment()
-	go increment()
-	go increment()
-	go increment()
-	go increment()
-	go increment()
-	go increment()
-
-	for {
-		c.L.Lock()
-		if counter >= interationAmount {
-			break
-		}
-		c.Signal()
-		c.L.Unlock()
-	}
-
+	// Ждем "сигнал"
+	c.L.Lock()
+	c.Wait()
+	c.L.Unlock()
 	fmt.Println(counter)
 }
-
-//func main() {
-//	var counter int64 = 0
-//	var c = sync.NewCond(&sync.Mutex{})
-//	increment := func(i int) {
-//		atomic.AddInt64(&counter, step)
-//		if i == interationAmount {
-//			c.Signal()
-//		}
-//	}
-//	for i := 1; i <= interationAmount; i++ {
-//		go increment(i)
-//	}
-//	c.L.Lock()
-//	c.Wait()
-//	c.L.Unlock()
-//	fmt.Println(counter)
-//}
