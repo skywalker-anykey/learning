054c8be2a379edb5618149953ec993fa3bf8ada7
Т Н <ntolik@gmail.com>
2024-05-26 18:50:07 +0300

    Исправил ошибку

--- src/HW17-3-1/main.go
+++ src/HW17-3-1/main.go
@@ -6,32 +6,30 @@ import (
 	"sync/atomic"
 )
 
-// Шаг наращивания счётчика
-const step int64 = 1
-
-// Конечное значение счетчика
-const endCounterValue int64 = 1000
-
-// Количество горутин
-const numGoroutines = 10
+const (
+	// Шаг наращивания счётчика
+	step int64 = 1
+	// Количество итераций на горутину
+	counterGoroutine = 100
+	// Количество горутин
+	numGoroutines = 10
+)
 
 func main() {
 
 	var counter int64 = 0
 	var wg sync.WaitGroup
-	increment := func() {
-		defer wg.Done()
-		for {
-			if counter >= endCounterValue {
-				return
-			}
-			atomic.AddInt64(&counter, step)
-		}
-	}
+
 	for i := 0; i < numGoroutines; i++ {
 		wg.Add(1)
-		go increment()
+		go func() {
+			defer wg.Done()
+			for i := 0; i < counterGoroutine; i++ {
+				atomic.AddInt64(&counter, step)
+			}
+		}()
 	}
+
 	// Ожидаем поступления сигнала
 	wg.Wait()
 	// Печатаем результат, надеясь, что будет 1000
