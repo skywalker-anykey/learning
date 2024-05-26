17598ddc4696d16c8991d47dc77deab928feb693
Т Н <ntolik@gmail.com>
2024-05-26 17:43:09 +0300

    Доработка по требованию

--- src/HW17-3-1/main.go
+++ src/HW17-3-1/main.go
@@ -12,27 +12,23 @@ const step int64 = 1
 // Конечное значение счетчика
 const endCounterValue int64 = 1000
 
+// Количество горутин
+const numGoroutines = 10
+
 func main() {
 
 	var counter int64 = 0
-	var counterWG int = 0
 	var wg sync.WaitGroup
 	increment := func() {
-		defer func() {
-			counterWG--
-			wg.Done()
-		}()
-		atomic.AddInt64(&counter, step)
-	}
-	// Не всегда вычисление этой переменной будет приводить к верному
-	// результату в счётчике, но для правильных значений
-	// и для удобства - можно
-	var iterationCount int = int(endCounterValue / step)
-	for i := 1; i <= iterationCount; i++ {
-		for counterWG > 10 {
-			wg.Wait()
+		defer wg.Done()
+		for {
+			if counter >= endCounterValue {
+				return
+			}
+			atomic.AddInt64(&counter, step)
 		}
-		counterWG++
+	}
+	for i := 0; i < numGoroutines; i++ {
 		wg.Add(1)
 		go increment()
 	}
