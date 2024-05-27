5239436df2509680edf7ca4a6b0ce58359732423
Т Н <ntolik@gmail.com>
2024-05-27 15:43:07 +0300

    Вариант решения 17.6.2

--- src/HW17-6-2/main.go
+++ src/HW17-6-2/main.go
@@ -1,7 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	var ticker *time.Ticker = time.NewTicker(time.Second * 1)
+	var t time.Time
+
+	for {
+		select {
+		case v := <-ch1:
+			fmt.Println("ch1:", v)
+		case v := <-ch2:
+			fmt.Println("ch2", v)
+		default:
+			t = <-ticker.C
+			outputMessage := []byte("Время: ")
+			outputMessage = t.AppendFormat(outputMessage, "15:04:05")
+			fmt.Println(string(outputMessage))
+		}
+	}
 }
 
 //Напишите код, в котором имеются два канала сообщений из целых чисел так, чтобы приём сообщений всегда приводил
