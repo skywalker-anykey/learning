4e680ec0b4aa42fb6bdb821a8429ce154cd6c8d8
Т Н <ntolik@gmail.com>
2024-05-27 16:12:43 +0300

    Вариант решения 17.6.3

--- src/HW17-6-3/main.go
+++ src/HW17-6-3/main.go
@@ -1,7 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	ch := make(chan int, 5)
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range ch {
+			fmt.Println(v)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 1; i < 101; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+
+	wg.Wait()
 }
 
 //Напишите программу, которая делает следующее: одна горутина по порядку отсылает числа от 1 до 100 в канал,
