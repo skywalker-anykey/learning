34be2a94dea8b535df0400fb3dd6d4093a3c132f
Т Н <ntolik@gmail.com>
2024-05-27 11:51:09 +0300

    Вариант решения 17.6.1

--- src/HW17-6-1/main.go
+++ src/HW17-6-1/main.go
@@ -1,7 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func receive(ch chan int, count *int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	wg.Add(1)
+	for range ch {
+		*count++
+	}
+}
+
+func main() {
+	// Количество отправляемых сообщений в каналы
+	const countSend = 100
+
+	var countC1, countC2 int
+	wg := sync.WaitGroup{}
+
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go receive(c1, &countC1, &wg)
+	go receive(c1, &countC1, &wg)
+	go receive(c2, &countC2, &wg)
+
+	for i := 0; i < countSend; i++ {
+		select {
+		case c1 <- i:
+		case c1 <- i:
+		case c2 <- i:
+		}
+	}
+	close(c1)
+	close(c2)
+	wg.Wait()
+	fmt.Println("Принятых сообщений первым каналом:", countC1)
+	fmt.Println("Принятых сообщений вторым каналом:", countC2)
 }
 
 //Напишите код, в котором имеются два канала сообщений из целых чисел, так,чтобы приём сообщений из них никогда
