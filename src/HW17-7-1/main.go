df72d7f570737c5852ae70332471e3b5078c4b45
Т Н <ntolik@gmail.com>
2024-05-29 19:26:26 +0300

    Вариант решения 17.7.1

--- src/HW17-7-1/main.go
+++ src/HW17-7-1/main.go
@@ -1,7 +1,92 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+const (
+	// numGoroutines - Количество используемых горутин
+	numGoroutines = 10
+	// goal - Цель конечного значения счетчика
+	goal = 1000
+)
+
+type Counter struct {
+	value  int
+	ch     chan int
+	goal   int
+	goalCh chan bool
+}
+
+func NewCounter() *Counter {
+	ch := make(chan int)
+	goalCh := make(chan bool)
+	value := 0
+
+	return &Counter{
+		value: value,
+		ch:    ch,
+
+		goalCh: goalCh,
+		goal:   goal,
+	}
+}
+
+func (c *Counter) Start() {
+	go func() {
+		for step := range c.ch {
+			if c.value < c.goal {
+				c.value += step
+				c.goalCh <- false
+			} else {
+				c.goalCh <- true
+			}
+		}
+	}()
+}
+
+func (c *Counter) Add(step int) bool {
+	c.ch <- step
+	if <-c.goalCh == true {
+		return true
+	}
+	return false
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
+
+func (c *Counter) Stop() {
+	close(c.ch)
+	close(c.goalCh)
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	c := NewCounter()
+
+	c.Start()
+
+	wg.Add(numGoroutines)
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			defer wg.Done()
+			for {
+				isGoal := c.Add(1)
+				if isGoal {
+					return
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	c.Stop()
+
+	fmt.Println(c.Value())
 }
 
 // Напишите код, в котором несколько горутин увеличивают значение целочисленного счётчика и синхронизируют свою работу
