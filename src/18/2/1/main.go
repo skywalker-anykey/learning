ce9a8d2ce96606127b2e00e962a0ff00d82a9d9a
Т Н <ntolik@gmail.com>
2024-05-31 15:21:34 +0300

    18.2.1 Переделана под старую версию

--- src/18/2/1/main.go
+++ src/18/2/1/main.go
@@ -1,3 +1,7 @@
+// 18.2.1 Создайте программу, которая запускает 5 рутин, каждая из которых печатает свой порядковый номер 10 раз.
+// Добиться такого результата за один вызов wg.Add.
+//
+// Используя старую версию Go
 package main
 
 import (
@@ -6,15 +10,21 @@ import (
 )
 
 func main() {
+	h := make(map[int]int)
 	wg := sync.WaitGroup{}
 	wg.Add(5)
 	for i := 0; i < 5; i++ {
-		go func() {
+		go func(i int) {
 			defer wg.Done()
 			for n := 0; n < 10; n++ {
-				fmt.Println(i)
+				val := i
+				fmt.Println(val)
+				h[val] = h[val] + 1
 			}
-		}()
+		}(i)
 	}
 	wg.Wait()
+	for key, v := range h {
+		fmt.Println(key, "выведено", v, "раз")
+	}
 }
