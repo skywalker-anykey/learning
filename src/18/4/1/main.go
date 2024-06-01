8102c82dc07701cb14bd5148cb8ce640ed52a233
Т Н <ntolik@gmail.com>
2024-06-01 15:03:52 +0300

    18.4.1 вариант 1

--- src/18/4/1/main.go
+++ src/18/4/1/main.go
@@ -34,13 +34,25 @@ func (s *Semaphore) Release() error {
 }
 
 func main() {
-	s := NewSemaphore(10, 3*time.Second)
+	s := NewSemaphore(3, 3*time.Second)
 
-	if err := s.Acquire(); err != nil {
-		fmt.Println(err)
+	// Выполним 9 задач по 1 с работы каждая с ограничением 3 потока одновременно
+	for i := 0; i < 9; i++ {
+		go func(i int) {
+			defer func() {
+				if err := s.Release(); err != nil {
+					fmt.Println(err)
+				}
+			}()
+			if err := s.Acquire(); err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Work", i, "Start")
+			time.Sleep(1 * time.Second)
+			fmt.Println("Work", i, "Stop")
+		}(i)
 	}
 
-	if err := s.Release(); err != nil {
-		fmt.Println(err)
-	}
+	// Должны завершить работы примерно за 3 секунды
+	time.Sleep(4 * time.Second)
 }
