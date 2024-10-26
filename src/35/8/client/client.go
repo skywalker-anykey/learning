2888ab066481c097d9915cb3c0572400d51e838d
Т Н <ntolik@gmail.com>
2024-10-26 19:40:15 +0300

    Done

--- src/35/8/client/client.go
+++ src/35/8/client/client.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+)
 
+type Point struct {
+	X, Y float64
+}
+
+type Points struct {
+	A, B Point
+}
+
+func main() {
+	client, err := rpc.DialHTTP("tcp4", "localhost:1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	var p Points = Points{
+		A: Point{X: 1, Y: 1},
+		B: Point{X: 4, Y: 5},
+	}
+
+	var resp float64
+
+	err = client.Call("Server.Dist", p, &resp)
+	if err != nil {
+		log.Println("call failed:", err)
+	}
+
+	fmt.Println("Dist:", resp)
 }
--- src/35/8/server/server.go
+++ src/35/8/server/server.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"log"
+	"math"
+	"net"
+	"net/http"
+	"net/rpc"
+)
 
+type Server int
+
+type Point struct {
+	X, Y float64
+}
+
+type Points struct {
+	A, B Point
+}
+
+func (s *Server) Dist(p Points, resp *float64) error {
+	*resp = (p.A.X-p.B.X)*(p.A.X-p.B.X) + (p.A.Y-p.B.Y)*(p.A.Y-p.B.Y)
+	*resp = math.Sqrt(*resp)
+	return nil
+}
+
+func main() {
+	srv := new(Server)
+	_ = rpc.Register(srv)
+	rpc.HandleHTTP()
+	listener, err := net.Listen("tcp4", ":1234")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = http.Serve(listener, nil)
 }
